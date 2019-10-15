7037844cf6f4e186ce95854927b9ddf211c7cafe
Sualeh Ali <smsali97@gmail.com>
2019-10-16 00:34:59 +0500

    Not working

---
+++ src/main/java/com/server/spring/ws/api/SpringBootWebsocketApplication.java
@@ -0,0 +1,16 @@
+package com.server.spring.ws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.server"})
+public class SpringBootWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/server/spring/ws/api/config/WsConfig.java
@@ -0,0 +1,30 @@
+package com.server.spring.ws.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import com.server.spring.ws.api.controller.WebSocketEventListener;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WsConfig implements WebSocketMessageBrokerConfigurer{
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("ws")
+		.setAllowedOrigins("*")
+		.withSockJS();
+		WebSocketEventListener.endpointCreated();
+	}
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/topic");
+		registry.setApplicationDestinationPrefixes("/app");
+	}
+	
+	
+}
